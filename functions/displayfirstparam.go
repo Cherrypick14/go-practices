8804ba19b21710a639d602b236386784a58f8e66
cowalla <rmiles957@gmail.com>
2024-05-15 11:06:45 +0300

    [Feat]: Add new displayfirst func

---
+++ functions/displayfirstparam.go
@@ -0,0 +1,25 @@
+package functions
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func Displayfirstparam() {
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		fmt.Println("Usage: <input> ")
+		return
+	}
+
+	input := args[0]
+
+	for _, char := range input {
+		z01.PrintRune(char)
+	}
+	z01.PrintRune('\n')
+
+}
