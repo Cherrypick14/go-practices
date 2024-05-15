2b4273f429920e4e0bbeb8c05b6c565ba68c76a0
cowalla <rmiles957@gmail.com>
2024-05-15 11:18:05 +0300

    [Feat] ; Add new displayalrevm

---
+++ functions/displayalrevm.go
@@ -0,0 +1,14 @@
+package functions
+
+import "github.com/01-edu/z01"
+
+func Displayalrevm() {
+	for i := 'Z'; i >= 'A'; i-- {
+		if i%2 == 0 {
+			z01.PrintRune(i + 32)
+		} else {
+			z01.PrintRune(i)
+		}
+	}
+	z01.PrintRune('\n')
+}
