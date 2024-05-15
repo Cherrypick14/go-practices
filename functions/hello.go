fb8826bd3a1f489687a535b759d24299ff303b9b
cowalla <rmiles957@gmail.com>
2024-05-15 11:07:49 +0300

    [Feat] : Add new hello func

---
+++ functions/hello.go
@@ -0,0 +1,14 @@
+package functions
+
+import "github.com/01-edu/z01"
+
+
+func Hello() {
+  z01.PrintRune('H')
+  z01.PrintRune('e')
+  z01.PrintRune('l')
+  z01.PrintRune('l')
+  z01.PrintRune('o')
+
+  z01.PrintRune('\n')
+}
