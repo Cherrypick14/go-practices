85df52f363cde2ce0e62e730bec1f10bf1380260
cowalla <rmiles957@gmail.com>
2024-05-15 10:23:31 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"practices/functions"
+	"time"
+
+)
+
+func main() {
+   functions.Displayfirstparam()
+
+   time.Sleep(2 *time.Second)
+
+   functions.Displaylastparam()
+
+   time.Sleep(2 *time.Second)
+
+   functions.Hello()
+
+
+}
