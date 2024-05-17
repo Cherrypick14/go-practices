d91d49855c5fddf9752b9f6df80eeeea52c41a4c
cowalla <rmiles957@gmail.com>
2024-05-17 09:21:43 +0300

    Format leap year with space

--- functions/leapyear.go
+++ functions/leapyear.go
@@ -3,7 +3,7 @@ package functions
 import "fmt"
 
 func Leapyear(num int) {
-	if num % 4 == 0 && (num % 100 != 0 || num % 400 ==0) {
+	if num % 4 == 0 && (num % 100 != 0 || num % 400 == 0) {
 		fmt.Println(true)
 	}else {
 		fmt.Println(false)
