5e719b1089efa0069b48b371609ef83bd97db95d
Cherrypick14 <cherrypicklet@gmail.com>
2024-06-03 23:00:46 +0300

    [Feat] Build foldin func

---
+++ functions/foldint.go
@@ -0,0 +1,21 @@
+package functions
+
+import "fmt"
+
+func FoldInt(f func(int, int) int, a []int, n int) {
+	for _, val := range a {
+		n = f(n, val)
+	}
+	fmt.Println(n)
+}
+func Add(a, b int) int {
+	return a + b
+}
+
+func Mul(a, b int) int {
+	return a * b
+}
+
+func Sub(a, b int) int {
+	return a - b
+}
