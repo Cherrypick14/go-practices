45d00a1e39813cced02b1bd60aef6e02541485e2
Cherrypick14 <cherrypicklet@gmail.com>
2024-06-03 23:45:48 +0300

    Add findprevprime

--- functions/isprime.go
+++ functions/isprime.go
@@ -1,10 +1,9 @@
 package functions
 
-
 func Isprime(n int) bool {
 	for i := 2; i*i <= n; i++ {
 		if n%i == 0 {
-			return  false
+			return false
 		}
 	}
 	return true
@@ -13,6 +12,17 @@ func Isprime(n int) bool {
 func FindNextPrime(n int) int {
 	if !Isprime(n) {
 		n++
+	}
+	return n
+}
+
+func FindPrevPrime(n int) int {
+	if n < 2 {
+		return -1
+	}
+
+	if !Isprime(n) {
+		n--
 	}
 	return n
 }
