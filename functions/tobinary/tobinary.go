68e7686b3750799863c5ad83740d57bac2ee75d1
Cherrypick14 <cherrypicklet@gmail.com>
2024-06-05 13:40:38 +0300

    Add binary func

---
+++ functions/tobinary/tobinary.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+
+func tobinary(n int) string{
+    res := ""
+
+	for n > 0 {
+		res = itoa4(n%2) + res
+		n = n/2
+	}
+	return res
+}
+
+func itoa4(nb int) string {
+	res := ""
+
+	if nb == 0 {
+		return "0"
+	}
+
+	for nb > 0 {
+		res = string(nb%10 + '0') + res
+		nb = nb/10
+	}
+	return res
+}
+func main(){
+  fmt.Println(tobinary(7))
+}
