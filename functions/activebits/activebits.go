2bca338c2fec77aa25fd019bee5a584b503a384c
Cherrypick14 <cherrypicklet@gmail.com>
2024-06-05 13:45:06 +0300

    Add activebit

---
+++ functions/activebits/activebits.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func activebits(nb int) int {
+	res := ""
+	count := 0
+	for nb > 0 {
+		if nb%2 == 1 {
+			count++
+		}
+		res = string(nb%2+'0') + res
+		nb = nb / 2
+	}
+	return count
+}
+
+func main() {
+	fmt.Println(activebits(7))
+}
