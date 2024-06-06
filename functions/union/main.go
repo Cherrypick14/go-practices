595012d63d4bc14091bc14a11fea4a98726e083c
Cherrypick14 <cherrypicklet@gmail.com>
2024-06-06 10:24:52 +0300

    Update unionfunc

--- functions/union/main.go
+++ functions/union/main.go
@@ -5,37 +5,44 @@ import (
 	"os"
 )
 
-func union(arr []string) string {
-	res := ""
-	mp := map[rune]rune{}
-	for _, val := range arr {
-		for _, char := range val {
-			_, ok := mp[char]
-			if !ok {
-				mp[char] = char
-				res += string(char)
-			}
-		}
-	}
-	return res
-}
-
-// func Union(a,b  string) string {
-//     var result string
-//     checker := map[rune]rune{}
-    
-//     for _,st :=range a+b{
-//         _,ok:=checker[st]
-//         if !ok{
-//             checker[st]=st
-//             result+=string(st)
-//         }
-//     }
-//     return result
+// func union(arr []string) string {
+// 	res := ""
+// 	mp := map[rune]rune{}
+// 	for _, val := range arr {
+// 		for _, char := range val {
+// 			_, ok := mp[char]
+// 			if !ok {
+// 				mp[char] = char
+// 				res += string(char)
+// 			}
+// 		}
+// 	}
+// 	return res
 // }
 
+func Union(a, b string) string {
+	var result string
+
+	checker := map[rune]rune{}
+
+	for _, char := range a + b {
+		_, ok := checker[char]
+		if !ok {
+			checker[char] = char
+			result += string(char)
+		}
+	}
+	return result
+}
+
 func main() {
-	args := os.Args[1:]
-	res := union(args)
+	if len(os.Args) != 3 {
+		fmt.Println()
+		 return
+		
+	}
+	args1 := os.Args[1]
+	args2 := os.Args[2]
+	res := Union(args1, args2)
 	fmt.Println(res)
 }
